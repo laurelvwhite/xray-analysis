9994df98d2d432cc2a6f0fe590238623ed3f4dfa
Laurel White <laurelwhite@dhcp-10-29-49-236.dyn.MIT.EDU>
2022-05-16 14:38:07 -0400

    Adding conversion tool for M500 to R500

---
+++ utils/convert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import argparse
+import astropy.constants as const
+import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--z', required=True,
+                    help='redshift')
+parser.add_argument('--M500', required=True,
+                    help='M500 in units of 10**14 Msun')
+
+args = parser.parse_args()
+
+z = args.z
+M500 = args.M500
+
+def M500toR500(z,m500):
+    z = float(z)
+    m500 = float(m500)
+    ## Calculate critical density at cluster redshift
+    H_0 = 70*3.24078*10**(-20)                                                   ## [s^-1]
+    Hubble = H_0*np.sqrt(0.3*(1+z)**3+0.7)                                       ## [s^-1]
+    pcrit = 3*Hubble**2/(8*np.pi*const.G.value)                                  ## [kg*m^-3]
+    ## Convert m500 to r500
+    r500 = (m500*10**14*const.M_sun.value/(500*pcrit)*(3/(4*np.pi)))**(1.0/3.0)  ## [m]
+    r500 *= 3.241*10**-20                                                        ## [kpc]
+    return r500
+
+print('R500 in kpc is {}'.format(M500toR500(z,M500)))
