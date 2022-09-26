ae5949a5cf02af44fa22abb806de9b9b1bfa89a5
Laurel White <laurelwhite@dhcp-10-29-78-18.dyn.MIT.EDU>
2022-09-26 16:50:15 -0400

    Finished writing code for aphot that exposure corrects images and backgrounds

--- cxo-pipe/python/aphot.py
+++ cxo-pipe/python/aphot.py
@@ -1,58 +1,108 @@
+import os
+
 import numpy as np
 
 from astropy.io import fits
 from ciao_contrib.runtool import *
 
 def expcorrect(indir,obsids):
+    outdir = indir + 'aphot/'
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
     if len(obsids)==1:
+        pt_src = indir + 'wide_wdect_expmap_src_check.reg'
+        corr_cts_img = outdir + 'corrected_counts.img'
+        corr_bkg_cts_img = outdir + 'corrected_bkg_counts.img'
         cts_img = indir + 'wide_broad_thresh.img'
         exp_map = indir + 'wide_broad_thresh.expmap'
-        pt_src = indir + 'wide_wdect_expmap_src_check.reg'
-        cts_img_no_pts = indir + 'counts_no_src.img'
-        corr_cts_img = indir + 'corrected_counts.img'
+        cts_img_no_pts = outdir + 'counts_no_src.img'
+        bkg_img = indir + 'blank_sky_{}.evt'.format(obsids[0])
 
         dmcopy.punlearn()
         dmcopy.infile = "{}[exclude sky=region({})]".format(cts_img,pt_src)
         dmcopy.outfile = cts_img_no_pts
+        dmcopy.clobber = 'yes'
         dmcopy()
 
         with fits.open(cts_img_no_pts) as f:
             arr = np.array(f[0].data)
             hdr = f[0].header
 
-        with fits.open(exp_map) as f:
+        blanksky_image.punlearn()
+        blanksky_image.bkgfile = bkg_img
+        blanksky_image.outroot = outdir + 'bkg_img'
+        blanksky_image.imgfile = cts_img_no_pts
+        blanksky_image.clobber = True
+        blanksky_image()
+
+        with fits.open(outdir + 'bkg_img_particle_bgnd.img') as f:
             bkg_arr = np.array(f[0].data)
+            bkg_hdr = f[0].header
 
-        corr_cts_obsid = arr / (bkg_arr/np.mean(bkg_arr))
+        with fits.open(exp_map) as f:
+            exp_arr = np.array(f[0].data)
 
+        corr_cts = arr / (exp_arr/np.mean(exp_arr))
+        corr_bkg_cts = bkg_arr / (exp_arr/np.mean(exp_arr))
+
+        fits.writeto(corr_cts_img, corr_cts, hdr, overwrite=True)
+        fits.writeto(corr_bkg_cts_img, corr_bkg_cts, bkg_hdr, overwrite=True)
     else:
-        corr_cts = []
+        pt_src = indir + 'wide_wdect_expmap_src_check.reg'
+        corr_cts_img = outdir + 'corrected_counts.img'
+        corr_bkg_cts_img = outdir + 'corrected_bkg_counts.img'
         for obsid in obsids:
             cts_img = indir + 'wide_{}_broad_thresh.img'.format(obsid)
             exp_map = indir + 'wide_{}_broad_thresh.expmap'.format(obsid)
-            pt_src = indir + 'wide_wdect_expmap_src_check.reg'
-            cts_img_no_pts = indir + '{}_counts_no_src.img'.format(obsid)
-            corr_cts_img_obsid = indir + '{}_corrected_counts.img'.format(obsid)
+            cts_img_no_pts = outdir + '{}_counts_no_src.img'.format(obsid)
+            corr_cts_img_obsid = outdir + '{}_corrected_counts.img'.format(obsid)
+            bkg_img = indir + 'blank_sky_{}.evt'.format(obsid)
+            corr_bkg_img_obsid = outdir + '{}_corrected_bkg.img'.format(obsid)
 
             dmcopy.punlearn()
             dmcopy.infile = "{}[exclude sky=region({})]".format(cts_img,pt_src)
             dmcopy.outfile = cts_img_no_pts
+            dmcopy.clobber = 'yes'
             dmcopy()
 
             with fits.open(cts_img_no_pts) as f:
                 arr = np.array(f[0].data)
                 hdr = f[0].header
 
-            with fits.open(exp_map) as f:
+            blanksky_image.punlearn()
+            blanksky_image.bkgfile = bkg_img
+            blanksky_image.outroot = outdir + '{}_bkg_img'.format()
+            blanksky_image.imgfile = cts_img_no_pts
+            blanksky_image.clobber = True
+            blanksky_image()
+
+            with fits.open(outdit + '{}_bkg_img_particle_bgnd.img'.format(obsid)) as f:
                 bkg_arr = np.array(f[0].data)
+                bkg_hdr = f[0].header
 
-            corr_cts_obsid = arr / (bkg_arr/np.mean(bkg_arr))
+            with fits.open(exp_map) as f:
+                exp_arr = np.array(f[0].data)
 
-            fits.writeto(corr_cts_img_obsid, corr_cts_obsid, hdr)
+            corr_cts_obsid = arr / (exp_arr/np.mean(exp_arr))
+            corr_bkg_obsid = bkg_arr / (exp_arr/np.mean(exp_arr))
+            fits.writeto(corr_cts_img_obsid, corr_cts_obsid, hdr, overwrite=True)
+            fits.writeto(corr_bkg_img_obsid, corr_bkg_obsid, bkg_hdr, overwrite=True)
 
+            if obsid == obsids[0]:
+                corr_cts = arr*0.0
+                corr_bkg_cts = bkg_arr*0.0
 
-indir = '../../results/PSZ2G021.10+33.24/results/'
-obsids = [6104,7940]
+            corr_cts += corr_cts_obsid
+            corr_bkg_cts += corr_bkg_obsid
+
+        fits.writeto(corr_cts_img, corr_cts, hdr, overwrite=True)
+        fits.writeto(corr_bkg_cts_img, corr_bkg_cts, bkg_hdr, overwrite=True)
+
+#indir = '../../results/PSZ2G021.10+33.24/results/'
+#obsids = [6104,7940]
+
+indir = '../../results/PSZ2G000.13+78.04/results/'
+obsids = [17159]
 
 expcorrect(indir,obsids)
 
